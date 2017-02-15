8d5413c274f3d6b825f0211eeed9e8369758da7f
Paul Scott <paul.scott.kildare@live.com>
2017-02-15 16:37:17 +0000

    First commit

---
+++ Stay Awake/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Stay_Awake {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.label1 = new System.Windows.Forms.Label();
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Stay Awake";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(93, 12);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Allow Sleep";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 60000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.DarkRed;
+			this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.label1.Location = new System.Drawing.Point(0, 44);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(182, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "OFF";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.BalloonTipText = "Stay Awake";
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "Stay Awake";
+			this.notifyIcon1.Visible = true;
+			this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(182, 57);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "Stay Awake";
+			this.Resize += new System.EventHandler(this.Form1_Resize);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+	}
+}
+
---
+++ Stay Awake/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Stay_Awake {
+	public partial class Form1 : Form {
+		[FlagsAttribute]
+		public enum EXECUTION_STATE : uint {
+			ES_AWAYMODE_REQUIRED = 0x00000040,
+			ES_CONTINUOUS = 0x80000000,
+			ES_DISPLAY_REQUIRED = 0x00000002,
+			ES_SYSTEM_REQUIRED = 0x00000001
+			// Legacy flag, should not be used.
+			// ES_USER_PRESENT = 0x00000004
+		}
+
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		static extern uint SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			timer1.Enabled = true;
+			label1.BackColor = Color.LimeGreen;
+			label1.Text = "ON";
+		}
+
+		private void timer1_Tick(object sender, EventArgs e) {
+			SetThreadExecutionState(EXECUTION_STATE.ES_SYSTEM_REQUIRED | EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+		}
+
+		private void button2_Click(object sender, EventArgs e) {
+			timer1.Enabled = false;
+			SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+			label1.BackColor = Color.DarkRed;
+			label1.Text = "OFF";
+		}
+
+		private void Form1_Resize(object sender, EventArgs e) {
+			if (FormWindowState.Minimized == this.WindowState) {
+				notifyIcon1.Visible = true;
+				notifyIcon1.ShowBalloonTip(500);
+				//this.Hide();
+				this.ShowInTaskbar = false;
+			}
+
+			else if (FormWindowState.Normal == this.WindowState) {
+				notifyIcon1.Visible = false;
+			}
+		}
+
+		private void notifyIcon1_MouseClick(object sender, MouseEventArgs e) {
+			this.WindowState = FormWindowState.Normal;
+			this.ShowInTaskbar = true;
+			notifyIcon1.Visible = false;
+		}
+	}
+}
